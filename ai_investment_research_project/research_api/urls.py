2143b161971543b926e31e9544920097b8e12626
Herald Park <herald1999@gmail.com>
2025-03-12 16:43:55 -0400

    Add .gitignore file

--- ai_investment_research_project/research_api/urls.py
+++ ai_investment_research_project/research_api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('stock-price/', views.get_stock_price_view, name='stock-price'),
     path('market-cap/', views.get_market_cap_view, name='market_cap'),
     path('pe-ratio/', views.get_pe_ratio_view, name='pe_ratio'),
+    path('basic-research/', views.run_basic_research_workflow_view, name='basic-research'),
 ]
--- ai_investment_research_project/research_api/views.py
+++ ai_investment_research_project/research_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .scraping import scrape_stock_price, scrape_market_cap, scrape_pe_ratio
+from .langgraph_workflow import basic_research_workflow, AgentState
 
 @api_view(['GET'])
 def hello_world(request):
@@ -35,3 +36,29 @@ def get_pe_ratio_view(request):
         return Response({"ticker": ticker, "PE Ratio (TTM)": pe_ratio})
     else: 
         return Response({"error": "Could not retrieve PE ratio"}, status=400)
+
+
+@api_view(['GET'])
+def run_basic_research_workflow_view(request):
+    ticker = request.GET.get('ticker', 'AAPL')
+
+    initial_state: AgentState = {
+        "ticker_symbol": ticker,
+        "stock_price": None,  
+        "market_cap": None,
+        "pe_ratio": None,
+        "summary": None
+    }
+    results = basic_research_workflow.invoke(initial_state)
+    final_state = results
+
+    response_data = {
+        "ticker": final_state["ticker_symbol"],
+        "stock_price": final_state["stock_price"],
+        "market_cap": final_state["market_cap"],
+        "pe_ratio": final_state["pe_ratio"],
+        "summary": final_state["summary"],
+    }
+
+    return Response(response_data)
+
